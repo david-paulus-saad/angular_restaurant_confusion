fc2d2b5f25c7d7e85fb728f55b5331648e0c96cb
David Poulis <david_fci@outlook.com>
2018-03-12 18:58:34 +0200

    RxJS Part2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,7 @@ import {Dish} from '../shared/dish';
 import {DishService} from '../services/dish.service';
 import { Params , ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import 'rxjs/add/operator/switchMap';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -10,15 +11,23 @@ import { Location } from '@angular/common';
 })
 export class DishdetailComponent implements OnInit {
 
- 
+  dishIds: number[];
+  prev: number;
+  next: number;
   dish : Dish;
   constructor(private dishservice: DishService 
   , private route: ActivatedRoute,
 private location: Location) { }
 
   ngOnInit() {
-    let id=+this.route.snapshot.params['id'];
-   this.dishservice.getDish(id).then(dish =>  this.dish =dish);
+    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds =dishIds);
+    this.route.params.switchMap((params: Params) =>this.dishservice.getDish(+params['id']))
+    .subscribe(dish => {this.dish =dish; this.setPrevNext(dish.id)});
+  }
+  setPrevNext(dishId: number){
+    let index =this.dishIds.indexOf(dishId);
+    this.prev =this.dishIds[(this.dishIds.length + index-1)%this.dishIds.length];
+    this.next=this.dishIds[(this.dishIds.length+index+1)%this.dishIds.length];
   }
   goBack(): void{
     this.location.back();
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import {leader} from '../shared/leader';
 import {LEADERS}from '../shared/leaders';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 @Injectable()
 export class LeaderService {
 
   constructor() { }
-  getLeaders(): Promise<leader[]> {
-    return Promise.resolve(LEADERS) ;
+  getLeaders(): Observable<leader[]> {
+    return Observable.of(LEADERS).delay(2000) ;
   }
-  getLeader(id: number): Promise<leader> {
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]) ;
+  getLeader(id: number): Observable<leader> {
+    return Observable.of(LEADERS.filter((leader) => (leader.id === id))[0]).delay(2000) ;
   }
-  getFeaturedLeader(): Promise<leader> {
-    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]) ;
+  getFeaturedLeader(): Observable<leader> {
+    return Observable.of(LEADERS.filter((leader) => leader.featured)[0]).delay(2000) ;
   }
 
 }
